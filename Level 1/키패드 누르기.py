6cdee47f55b45d194f38194e5a646062aec8e178
JangJongMin <payload.jang@gmail.com>
2022-05-25 23:20:31 +0900

    키패드 누르기 Description 추가

--- Level 1/키패드 누르기.py
+++ Level 1/키패드 누르기.py
@@ -40,6 +40,10 @@ def solution(numbers, hand):
                 hands[0] = number
     return answer
 
-get_distance(['2', '3'], 5)
-
 # URL : https://programmers.co.kr/learn/courses/30/lessons/67256
+# 문제를 요약하면 1, 4, 7 *는 무조건 왼쪽 손가락이 3, 6, 9, #은  오른쪽 손가락을 사용하게 됩니다.
+# 마지막으로 2, 5, 8, 0는 손가락 이동 거리를 계산해 더 적게 이동하는 손가락이 두 손의 거리가 동일하다면 본인이 사용하기 편한 손가락이 이동합니다.
+# left_keypad와 right_keypad를 설정한 뒤 20~25 Line에서 mid_keypad가 아닌 경우를 처리합니다.
+# 마지막으로 mid_keypad인 경우 get_distance 함수를 통해 거리를 계산합니다.
+# get_distance 함수의 경우 왼쪽 손과 오른쪽 손의 위치가 mid_keypad에 있지 않다면 무조건 1칸 이동해야 함으로 8~11 Line에서 index를 증가시켜줍니다.
+# 좌우로 이동한 거리를 확인했으면 마지막으로 상하로 이동하는 거리를 12~13 Line과 같이 추가해주면 두 손가락의 거리를 확인할 수 있습니다.
