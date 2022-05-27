6919b60af4b8a7ac7c8a33db290b43b121e7e473
JangJongMin <payload.jang@gmail.com>
2022-05-27 15:25:24 +0900

    내적

---
+++ Level 1/내적.py
@@ -0,0 +1,8 @@
+import numpy
+
+def solution(a, b):
+    return int(numpy.dot(a,b))
+
+# https://programmers.co.kr/learn/courses/30/lessons/70128
+# Programmers, Level1, 내적, 월간 코드 챌린지 시즌1
+# 내적 계산은 numpy라이브러리를 이용하면 쉽게 할 수 있으며 해당 문제에서 int형으로 반환해야 정답으로 인식합니다.
