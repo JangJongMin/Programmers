9a6c89f742e1637cdfa1f54136fb70b8c6701635
JangJongMin <payload.jang@gmail.com>
2022-06-26 20:48:34 +0900

    description

--- Level 1/폰켓몬.py
+++ Level 1/폰켓몬.py
@@ -3,4 +3,6 @@ from collections import Counter
 def solution(nums):
     return int(len(nums)/2) if int(len(nums)/2) < len(Counter(nums)) else len(Counter(nums))
 
-# https://programmers.co.kr/learn/challenges
+# https://programmers.co.kr/learn/courses/30/lessons/1845
+# Programmers, Level1, 폰켓몬, 찾아라 프로그래밍 마스터
+# 다시 말해 폰켓몬 개수/2를 한 결과가 종류보다 많으면 모든 폰켓몬을 아닐 경우 폰켓몬 개수 만큼을 가질 수 있습니다.
