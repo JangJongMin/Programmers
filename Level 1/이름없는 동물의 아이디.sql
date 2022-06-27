6619317a1d400ff278c96ccb992ba9a53535d1f8
JangJongMin <payload.jang@gmail.com>
2022-06-28 02:12:03 +0900

    Description

--- Level 1/이름없는 동물의 아이디.sql
+++ Level 1/이름없는 동물의 아이디.sql
@@ -1,4 +1,4 @@
 SELECT ANIMAL_ID from ANIMAL_INS where NAME is NULL
 
 -- https://programmers.co.kr/learn/courses/30/lessons/59039
--- Programmers, Level1, 이름없는 동물의 아이디, ISNULL
+-- Programmers, Level1, 이름이 없는 동물의 아이디, ISNULL
